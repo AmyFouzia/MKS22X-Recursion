ca17daa13e1860b42d2c2886df7ba5c1bd87e15b
fuadbinzia <afouzia00@stuy.edu>
2019-02-03 18:41:50 -0500

    added base case on fib and formatted helper, listed helpful notes

--- recursion.java
+++ recursion.java
@@ -34,12 +34,28 @@ public class recursion{
 
     //NUMBER TWO
     /*Recursively find the n'th fibbonaci number in linear time
-     *fib(0) = 1; fib(1) = 1; fib(5) = 5
+     *fib(0) = 1;
+     fib(1) = 1;
+     fib(5) = 5
      *precondition: n is non-negative
     */
 
     public static int fib(int n){
+        //base case
+        if (n == 0 || n == 1) {return 1;}
+        //helper required to continue
+    }
 
+    //the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+    //The next number is found by adding up the two numbers before it.
+
+    public static int fibHelp(int n, int twoBehind, int oneBehind){
+        if(){
+
+        }
+        else{
+          
+        }
     }
 
     //NUMBER THREE
