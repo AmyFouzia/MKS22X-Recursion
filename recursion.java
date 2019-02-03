068cb0e2f40de5bbf89a8acb0f1c526519f53c6f
fuadbinzia <afouzia00@stuy.edu>
2019-02-03 18:27:07 -0500

    some formatting and added param to helper sqrt

--- recursion.java
+++ recursion.java
@@ -1,6 +1,6 @@
 public class recursion{
-    /*You may write additional private methods */
 
+    //NUMBER ONE
     /*Recursively find the sqrt using Newton's approximation
      *tolerance is the allowed percent error the squared answer is away from n.
      *precondition: n is non-negative
@@ -10,23 +10,31 @@ public class recursion{
         //base case
         if (n == 0) {return 0;}
         //requires helper to continue
+        else{
+          return
+        }
     }
 
-    public static double sqrtHelp(double n, double tolerance){
-        
+    public static double sqrtHelp(double n, double tolerance, double guess){
+        if(){
+          return
+        }
+        else{
+          return guess;
+        }
     }
 
-
+    //NUMBER TWO
     /*Recursively find the n'th fibbonaci number in linear time
      *fib(0) = 1; fib(1) = 1; fib(5) = 5
      *precondition: n is non-negative
-     */
+    */
 
     public static int fib(int n){
 
     }
 
-    /*As Per classwork*/
+    //NUMBER THREE
 
     public static ArrayList<Integer> makeAllSums(){
     }
