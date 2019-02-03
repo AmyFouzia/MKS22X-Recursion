73cdcf26374e94ea3a75a384601754f3ab0c13f9
fuadbinzia <afouzia00@stuy.edu>
2019-02-03 18:34:52 -0500

    listed some helpful notes, added in helper method code

--- recursion.java
+++ recursion.java
@@ -15,9 +15,17 @@ public class recursion{
         }
     }
 
+    //( n / guess + guess) / 2 --  to make better guess
+    //Math.pow -- squaring guess
+    //test accuracy by squaring the guess, NOT by using another sqrt method!!!
+    //This can be done with a while loop until your guess squared is close to n by a % difference of some threshhold. (0.001% is not bad which is 0.00001)
+    //*@param n any non-negative value you want to take the sqrt of
+    //*@return the approximate sqrt of n within a tolerance of 0.001%
+    //Write and TEST the method:
+
     public static double sqrtHelp(double n, double tolerance, double guess){
-        if(){
-          return
+        if((Math.pow(guess, 2) - n) / n > tolerance){ //checking percent error of guess squared
+          return sqrtHelp(n, tolerance, (n / guess + guess) / 2); //using reccomended method of making a better guess
         }
         else{
           return guess;
